30e96c0463dee7c87e56756865a7e3f2cc224b16
Willem Liu <willem_liu@hotmail.com>
2017-11-14 14:03:06 +0100

    Improved test

--- test/ImageButton.test.tsx
+++ test/ImageButton.test.tsx
@@ -1,8 +1,16 @@
+import * as Enzyme from 'enzyme';
+import * as Adapter from 'enzyme-adapter-react-16';
+import { shallow, mount } from 'enzyme';
+
 import * as React from 'react';
 import * as renderer from 'react-test-renderer';
 import ImageButton from "../src/ImageButton";
 
 console.info = function() {};
+
+beforeAll(() => {
+    Enzyme.configure({ adapter: new Adapter() });
+});
 
 test('ImageButton renders correctly', () => {
     const component = renderer.create(
@@ -10,4 +18,14 @@ test('ImageButton renders correctly', () => {
     );
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
+});
+
+test('ImageButton buttons clicked', () => {
+    const spy = jest.fn();
+    let component = mount(
+        <ImageButton onClick={spy} alt={"alt-text"} src={'test.png'} className={'css-class-name'}/>
+    );
+
+    component.simulate('click');
+    expect(spy).toHaveBeenCalled();
 });
