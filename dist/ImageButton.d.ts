27870290333d75ac403d2916ce1409523124fa95
Willem Liu <willem_liu@hotmail.com>
2017-11-16 08:50:37 +0100

    v1.0.12 Added types

---
+++ dist/ImageButton.d.ts
@@ -0,0 +1,14 @@
+/// <reference types="react" />
+import * as React from 'react';
+/**
+ * Renders an image with an onClick hook for parent component.
+ */
+export default class ImageButton extends React.Component {
+    state: any;
+    props: any;
+    constructor(props: any);
+    componentDidMount(): void;
+    componentWillUnmount(): void;
+    componentWillReceiveProps(nextProps: any): void;
+    render(): JSX.Element;
+}
